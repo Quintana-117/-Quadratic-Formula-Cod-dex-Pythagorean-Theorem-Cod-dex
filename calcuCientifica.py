5be88393971733f9a67a8262b4eaeb7a49f4c832
Quintana-117 <hiramquintanasoto@gmail.com>
2025-03-12 05:57:57 +0000

    fin

---
+++ calcuCientifica.py
@@ -0,0 +1,20 @@
+# Pythagorean Theorem 📐
+# Codédex
+""" a = int(input("ingresa un numero: "))
+b = int(input("ingresa otro numero: "))
+
+c = (a**2 + b**2) ** 0.5
+print(c) """
+
+# Quadratic Formula 🧮
+# Codédex
+
+a = int(input("ingrese a: "))
+b = int(input("ingrese b: "))
+c = int(input("ingrese c: "))
+
+root1 = (-b + (b*b - 4*a*c)**0.5) / (2*a)
+root2 = (-b - (b*b - 4*a*c)**0.5) / (2*a)
+
+print(root1)
+print(root2)
